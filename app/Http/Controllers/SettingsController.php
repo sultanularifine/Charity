7323975f2a9cb983f2f97ba7c8f5bebba362623e
arifine <sultanularifine@gmail.com>
2024-03-08 23:40:33 +0600

    blog, basic , banner done

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HeroImage;
+use App\Models\Basic;
+use Illuminate\Http\Request;
+
+class SettingsController extends Controller
+{
+    public function basic()
+    {
+        $basic = Basic::first();
+        return view('fontend.settings.basic', ['basics' => $basic]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'site_title' => 'required|string',
+            'site_tagline' => 'string',
+            'footer_text.*' => 'required|string',
+            'phone' => 'string',
+            'email' => 'string',
+            'facebook' => 'string',
+            'instagram' => 'string',
+            'twitter' => 'string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif',
+        ]);
+
+        $basic = Basic::first();
+        if (empty($basic)) {
+            $basic = new Basic();
+        }
+        $basic->site_title = $request->site_title;
+        $basic->site_tagline = $request->site_tagline;
+        $basic->footer_text = $request->footer_text;
+        $basic->phone = $request->phone;
+        $basic->email = $request->email;
+        $basic->facebook = $request->facebook;
+        $basic->instagram = $request->instagram;
+        $basic->twitter = $request->twitter;
+        if ($request->hasFile('image')) {
+            if ($basic->image) {
+                $oldimagePath = public_path($basic->image);
+                if (file_exists($oldimagePath)) {
+                    unlink($oldimagePath);
+                }
+            }
+            $imageFile = $request->file('image');
+            $imageName = time() . '_' . $imageFile->getClientOriginalName();
+            $imageFile->move(public_path('fontend/logos'), $imageName);
+            $basic->image = 'logos/' . $imageName;
+        }
+        if ($basic->save()) {
+            return redirect()->route('settings.basic');
+        }
+    }
+
+    public function banner()
+    {
+        $heroimages = HeroImage::all();
+        return view('fontend.settings.banner', ['heroimages' => $heroimages]);
+    }
+    public function heroImageStore(Request $request)
+    {
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif',
+        ]);
+
+        $heroimages = new HeroImage();
+        if ($request->hasFile('image')) {
+            $imageFile = $request->file('image');
+            $imageName = time() . '_' . $imageFile->getClientOriginalName();
+            $imageFile->move(public_path('fontend/heroImages'), $imageName);
+            $heroimages->image = 'heroImages/' . $imageName;
+        }
+        if ($heroimages->save()) {
+            return redirect()->route('settings.banner');
+        }
+    }
+    public function heroImageDestroy($id)
+    {
+        $heroimages = HeroImage::find($id);
+        if ($heroimages->delete()) {
+            return redirect()->route('settings.banner');
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\SettingsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::redirect('/', '/admin/dashboard');
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [BlogController::class, 'dashboard'])->name('dashboard');
+    //Blog Controller
+    Route::prefix('blog')->group(function () {
+        Route::get('/list', [BlogController::class, 'index'])->name('blog.list');
+        Route::get('/create', [BlogController::class, 'create'])->name('blog.create');
+        Route::post('/store', [BlogController::class, 'store'])->name('blog.store');
+        Route::get('/show/{id}', [BlogController::class, 'show'])->name('blog.show');
+        Route::get('/edit/{id}', [BlogController::class, 'edit'])->name('blog.edit');
+        Route::put('/update/{id}', [BlogController::class, 'update'])->name('blog.update');
+        Route::delete('/delete/{id}', [BlogController::class, 'destroy'])->name('blog.destroy');
+    });
+    //Settings Controller
+    Route::prefix('settings')->group(function () {
+        Route::get('/basic', [SettingsController::class, 'basic'])->name('settings.basic');
+        Route::post('/basic', [SettingsController::class, 'store'])->name('settings.store');
+        Route::get('/banner', [SettingsController::class, 'banner'])->name('settings.banner');
+        Route::post('/banner', [SettingsController::class, 'heroImageStore'])->name('settings.imageStore');
+        Route::delete('/banner/{id}', [SettingsController::class, 'heroImageDestroy'])->name('settings.destroy');
+    });
+});
